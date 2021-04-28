9921f29e6f63b051ac63c43acb2511251a672235
Radrick <radrickgomes2708@hotmail.com>
2021-04-27 22:47:03 -0300

    add app folder

---
+++ app/src/main/java/com/example/tradermanager/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tradermanager.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tradermanager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
