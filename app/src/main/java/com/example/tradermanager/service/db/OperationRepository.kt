fe438a68bd5ffad5f84b93614b1177eb06e0c917
Radrick <radrickgomes2708@hotmail.com>
2021-05-12 21:12:12 -0300

    finished repository class

--- app/src/main/java/com/example/tradermanager/service/db/OperationRepository.kt
+++ app/src/main/java/com/example/tradermanager/service/db/OperationRepository.kt
@@ -10,17 +10,17 @@ class OperationRepository private constructor(context: Context) {
     private val mDataBaseHelper: DataBaseHelper = DataBaseHelper(context)
 
     //singleton partiner
-    companion object{
+    companion object {
         private lateinit var mRepository: OperationRepository
-        fun getInstance(context: Context): OperationRepository{
-            if (!Companion::mRepository.isInitialized){
+        fun getInstance(context: Context): OperationRepository {
+            if (!Companion::mRepository.isInitialized) {
                 mRepository = OperationRepository(context)
             }
             return mRepository
         }
     }
 
-    fun save(operation: OperationModel): Boolean{
+    fun save(operation: OperationModel): Boolean {
         try {
             val db = mDataBaseHelper.writableDatabase
             val contentValue = ContentValues().apply {
@@ -33,12 +33,12 @@ class OperationRepository private constructor(context: Context) {
             db.insert(OPERATION.TABLE_NAME, null, contentValue)
             db.close()
             return true
-        } catch (e: Exception){
+        } catch (e: Exception) {
             return false
         }
     }
 
-    fun update(operation: OperationModel): Boolean{
+    fun update(operation: OperationModel): Boolean {
         try {
             val db = mDataBaseHelper.writableDatabase
             val contentValues = ContentValues().apply {
@@ -53,27 +53,78 @@ class OperationRepository private constructor(context: Context) {
             db.update(OPERATION.TABLE_NAME, contentValues, selection, args)
             db.close()
             return true
-        } catch (e: Exception){
+        } catch (e: Exception) {
             return false
         }
     }
 
-    fun delete(id: Int): Boolean{
+    fun delete(id: Int): Boolean {
         try {
             val db = mDataBaseHelper.writableDatabase
             val selection = OPERATION.COLUMNS.ID + " = ? "
             val args = arrayOf(id.toString())
-            db.delete(OPERATION.TABLE_NAME, selection,args)
+            db.delete(OPERATION.TABLE_NAME, selection, args)
             db.close()
             return true
-        } catch (e: Exception){
+        } catch (e: Exception) {
             return false
         }
     }
 
-    fun listAll(id: Int): List<OperationModel>?{
-        var operationList: MutableList<OperationModel>? = ArrayList()
+    fun listAll(id: Int): List<OperationModel>? {
+        var operationList: MutableList<OperationModel> = ArrayList()
+
+        try {
+            val db = mDataBaseHelper.readableDatabase
+            val projection = arrayOf(
+                OPERATION.COLUMNS.ID,
+                OPERATION.COLUMNS.INPUT_POINT,
+                OPERATION.COLUMNS.OUTPUT_POINT,
+                OPERATION.COLUMNS.QUANTITY_CONTRACT,
+                OPERATION.COLUMNS.RESULT_POINT,
+                OPERATION.COLUMNS.RESULT_FINANCE
+            )
+
+            val cursor = db.query(
+                OPERATION.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+            )
+
+            if (cursor != null && cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val id: Int = cursor.getInt(cursor.getColumnIndex(OPERATION.COLUMNS.ID))
+                    val inputPoint: Int =
+                        cursor.getInt(cursor.getColumnIndex(OPERATION.COLUMNS.INPUT_POINT))
+                    val outputPoint: Int =
+                        cursor.getInt(cursor.getColumnIndex(OPERATION.COLUMNS.OUTPUT_POINT))
+                    val quantityContract: Int =
+                        cursor.getInt(cursor.getColumnIndex(OPERATION.COLUMNS.QUANTITY_CONTRACT))
+                    val resultPoint: Int =
+                        cursor.getInt(cursor.getColumnIndex(OPERATION.COLUMNS.RESULT_POINT))
+                    val resultFinance: Double =
+                        cursor.getDouble(cursor.getColumnIndex(OPERATION.COLUMNS.RESULT_FINANCE))
+
+                    val operation = OperationModel(
+                        id,
+                        inputPoint,
+                        outputPoint,
+                        quantityContract,
+                        resultPoint,
+                        resultFinance
+                    )
+                    operationList.add(operation)
+                }
+            }
+            cursor.close()
+            db.close()
+        } catch (e: Exception) {
+            operationList
+        }
         return operationList
     }
-
 }
